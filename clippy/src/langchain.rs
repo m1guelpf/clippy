8c5162c539f55a0c8b7a2819afe24022aa133996
Miguel Piedrafita <github@miguelpiedrafita.com>
2023-02-03 04:24:56 +0000

    wip

--- clippy/src/langchain.rs
+++ clippy/src/langchain.rs
@@ -6,10 +6,9 @@ use crate::qdrant::PointResult;
 #[must_use]
 pub fn build_prompt(query: &str, sources: &[PointResult]) -> String {
     formatdoc!(
-        "Given the following extracted parts of a project's documentation and a question, create a final helpful answer with references (\"sources\") to the 2 most relevant sources.
-        If you don't know the answer, just answer that you don't know and provide an empty sources array. Don't try to make up an answer.
-        ALWAYS return a \"sources\" JSON key in your answer with the most relevant paths.
-
+        "Given the following extracts of a project's documentation, create a helpful answer to the provided question.
+        ALWAYS return a JSON object with an \"answers\" key and a \"sources\" key containing the most relevant paths. Provide at max 2 sources.
+        If you don't know the answer, just answer that you don't know and provide an empty sources array. Don't try to make up an answer and don't answer questions unrelated to the project.
     INPUT: {}
     OUTPUT:",
         json!({
